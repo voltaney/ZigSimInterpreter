d04c9144110e9037730d04fc00a77d7ba528fbbf
voltaney <voltaney61@gmail.com>
2025-02-14 09:16:33 +0900

    名前空間の整理とデータ構造の統合
    各ソースファイルから `using` ステートメントを削除し、`ZigSimInterpreter.Models.SensorDataComponents` という新しい名前空間を定義しました。この名前空間内に、`CompassData`、`GPS`、`LightData`、`MicLevel`、`PressureData`、`ProximityMonitorData`、`Touch` の各データ構造をまとめました。これにより、コードの整理が進み、可読性が向上しました。

--- ZigSimInterpreter/Models/SensorDataComponents/GPS.cs
+++ ZigSimInterpreter/Models/SensorDataComponents/GPS.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ZigSimInterpreter.Models.SensorDataComponents
+﻿namespace ZigSimInterpreter.Models.SensorDataComponents
 {
     public record GPS(
         double Latitude,
